d01dd937bc0d0f862ccf1a5177b64afb1999ebcc
Danna Victoria <aagd090827mmcllna1@lake.edu.mx>
2025-04-09 11:18:34 -0600

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>⋆˚࿔ 𝐏𝐨𝐤𝐞𝐝𝐞𝐱-𝐃𝐚𝐧𝐧𝐚 𝜗𝜚˚⋆</h1>
+      <h2>𝒲𝑒𝓁𝒸𝑜𝓂𝑒 𝓉𝑜 𝓉𝒽𝑒 𝓅𝑜𝓀𝑒𝒹𝑒𝓍 ;𝟑</h2>
+      <p>you can find information about your favorite pokemon</p>
+
+
+      <div>
+        <p>Developed by Danna Victoria</p>
+        <p>𝟐𝟎𝟐𝟓</p>
+        <p>All rights reserverd</p>
+
+
+
+      </div>
+    </>
+  )
+}
+
+export default App
